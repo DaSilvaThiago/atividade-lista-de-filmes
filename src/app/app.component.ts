0324d555f29653ff902e2870bd8bdb4e22bed161
Thiago <silvathiago011@hotmail.com>
2023-11-24 21:45:46 -0300

    soso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,9 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Cristian-filmes';
+  title = 'ThiagoZika';
   categoriaSelecionada: string = '';
+  categorias: string[] = ['Ação', 'Romance', 'Comédia', 'Drama', 'Ficção Científica'];
 
   listaDeFilmes = [
     {categoria: 'Ação', titulo: 'Rambo',urlImagem:''},
@@ -23,28 +24,25 @@ export class AppComponent {
     {categoria: 'Ação', titulo: 'Mad Max: Estrada da Fúria', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Ação', titulo: 'Velozes e Furiosos', urlImagem: 'URL_DA_IMAGEM'},
 
-    // Romance
     {categoria: 'Romance', titulo: 'Orgulho e Preconceito', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Romance', titulo: 'A Culpa é das Estrelas', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Romance', titulo: 'Cidade dos Anjos', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Romance', titulo: 'Diário de uma Paixão', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Romance', titulo: 'La La Land', urlImagem: 'URL_DA_IMAGEM'},
 
-    // Comédia
     {categoria: 'Comédia', titulo: 'Se Beber, Não Case', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Comédia', titulo: 'Superbad', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Comédia', titulo: 'Esqueceram de Mim', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Comédia', titulo: 'As Branquelas', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Comédia', titulo: 'Ace Ventura: Pet Detective', urlImagem: 'URL_DA_IMAGEM'},
 
-    // Drama
     {categoria: 'Drama', titulo: 'O Resgate do Soldado Ryan', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Drama', titulo: 'Clube da Luta', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Drama', titulo: 'Forrest Gump', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Drama', titulo: 'A Lista de Schindler', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Drama', titulo: 'O Pianista', urlImagem: 'URL_DA_IMAGEM'},
 
-    // Ficção Científica
+
     {categoria: 'Ficção Científica', titulo: 'Blade Runner', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Ficção Científica', titulo: 'Interstellar', urlImagem: 'URL_DA_IMAGEM'},
     {categoria: 'Ficção Científica', titulo: 'O Quinto Elemento', urlImagem: 'URL_DA_IMAGEM'},
