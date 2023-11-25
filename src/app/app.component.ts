d123c979d3637b6e59238565aef0c097b6285f88
THIAGO DIAS DA SILVA <thiago.dsilva10@senacsp.edu.br>
2023-11-24 21:31:32 -0300

    soso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,62 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ThiagoZika';
+  title = 'Cristian-filmes';
+  categoriaSelecionada: string = '';
+
+  listaDeFilmes = [
+    {categoria: 'Ação', titulo: 'Rambo',urlImagem:''},
+    {categoria: 'Romance', titulo: 'Titanic',urlImagem:''},
+    {categoria: 'Ação', titulo: 'Matrix', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ação', titulo: 'Duro de Matar', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ação', titulo: 'Vingadores: Ultimato', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ação', titulo: 'Mad Max: Estrada da Fúria', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ação', titulo: 'Velozes e Furiosos', urlImagem: 'URL_DA_IMAGEM'},
+
+    // Romance
+    {categoria: 'Romance', titulo: 'Orgulho e Preconceito', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Romance', titulo: 'A Culpa é das Estrelas', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Romance', titulo: 'Cidade dos Anjos', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Romance', titulo: 'Diário de uma Paixão', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Romance', titulo: 'La La Land', urlImagem: 'URL_DA_IMAGEM'},
+
+    // Comédia
+    {categoria: 'Comédia', titulo: 'Se Beber, Não Case', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Comédia', titulo: 'Superbad', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Comédia', titulo: 'Esqueceram de Mim', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Comédia', titulo: 'As Branquelas', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Comédia', titulo: 'Ace Ventura: Pet Detective', urlImagem: 'URL_DA_IMAGEM'},
+
+    // Drama
+    {categoria: 'Drama', titulo: 'O Resgate do Soldado Ryan', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Drama', titulo: 'Clube da Luta', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Drama', titulo: 'Forrest Gump', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Drama', titulo: 'A Lista de Schindler', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Drama', titulo: 'O Pianista', urlImagem: 'URL_DA_IMAGEM'},
+
+    // Ficção Científica
+    {categoria: 'Ficção Científica', titulo: 'Blade Runner', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ficção Científica', titulo: 'Interstellar', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ficção Científica', titulo: 'O Quinto Elemento', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ficção Científica', titulo: 'De Volta para o Futuro', urlImagem: 'URL_DA_IMAGEM'},
+    {categoria: 'Ficção Científica', titulo: 'Matrix Reloaded', urlImagem: 'URL_DA_IMAGEM'}
+
+  ];
+
+  get filmesFiltrados() {
+    return this.listaDeFilmes.filter(filme => filme.categoria.toLowerCase() === this.categoriaSelecionada.toLowerCase());
+  }
+
+  atualizarCategoriaSelecionada(categoria: string) {
+    this.categoriaSelecionada = categoria;
+  }
 }
